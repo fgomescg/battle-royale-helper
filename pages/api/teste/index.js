49cca519e4bf650ca2e0c3a46d521676edb1589a
Fábio Gomes <fgomescg@gmail.com>
2021-02-18 23:07:00 -0300

    Teste git

--- pages/api/teste/index.js
+++ pages/api/teste/index.js
@@ -3,7 +3,7 @@ export default async function Teste(request, response) {
     response.status(200);
     response.json(
         {
-            "date": Date.now(),
+            "date": Date.now().toLocaleString(),
             "nome": "TESTE"            
         }
     )
